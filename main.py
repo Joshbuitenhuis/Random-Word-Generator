76ea3937e0c3ff91c27b5bd7a3e9ccf8123cc81e
Thatoddone <66497499+Thatoddone1@users.noreply.github.com>
2020-09-16 17:14:04 -0400

    Update main.py

--- main.py
+++ main.py
@@ -10,8 +10,3 @@ import random
 print("A random number from list is : ", end="")
 print(random.choice(["trash bin", "hamer", "window", "mouse", "brick"]))
 
-# using randrange() to generate in range from 20
-# to 50. The last parameter 3 is step size to skip
-# three numbers when selecting.
-print("A random number from range is : ", end="")
-print(random.randrange(20, 50, 3)) 
