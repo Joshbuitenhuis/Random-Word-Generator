a8bd9b835b26090e7098c628ed876bba6a107a58
Thatoddone <66497499+Thatoddone1@users.noreply.github.com>
2020-09-16 17:10:14 -0400

    Update main.py

--- main.py
+++ main.py
@@ -6,12 +6,17 @@ from tkinter import *
 root = Tk()
 root.title("Random Genarater")
 
-def ranword ():
+
+
+
+def ranwordgen ():
     # using choice() to generate a random word from a
     # given list.
+    ranword = random.choice(["trash bin", "hamer", "window", "mouse", "brick"])
     print(random.choice(["trash bin", "hamer", "window", "mouse", "brick"]))
-    ranwordtext = Label(root, text=random.choice(["trash bin", "hamer", "window", "mouse", "brick"]))
+    ranwordtext = Label(root, text=ranword)
     ranwordtext.grid(row=1, column=0)
+
 #defines button
-startbutton = Button(root, text="Genarate Random Word", height = 10, width =30,command=ranword)
+startbutton = Button(root, text="Genarate Random Word", height = 10, width =30,command=ranwordgen)
 startbutton.grid(row=0, column=0)
