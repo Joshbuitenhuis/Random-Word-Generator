e6c144da5792c7bffe1e24f3a477cf67d5e4065b
Thatoddone <66497499+Thatoddone1@users.noreply.github.com>
2020-09-16 07:43:38 -0400

    added the random genarater part

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
-print ("Hello World")
+
+import random
+
+
+
+
+
+# using choice() to generate a random number from a
+# given list.
+print("A random number from list is : ", end="")
+print(random.choice(["trash bin", "hamer", "window", "mouse", "brick"]))
+
+# using randrange() to generate in range from 20
+# to 50. The last parameter 3 is step size to skip
+# three numbers when selecting.
+print("A random number from range is : ", end="")
+print(random.randrange(20, 50, 3))
