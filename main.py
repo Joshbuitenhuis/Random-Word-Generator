95f7c3f88459e848116f69cf9247a1307915bb25
Thatoddone <66497499+Thatoddone1@users.noreply.github.com>
2020-09-16 20:24:46 -0400

    made it so you can use it in console and gui

--- main.py
+++ main.py
@@ -1,30 +1,43 @@
 
 import random
 from tkinter import *
-
-
 root = Tk()
 root.title("Random Genarater")
 root.geometry("200x500")
 
 
-
-
-
+a = 1
+def consoleran():   
+    while a == 1:
+        print("A random word is : ", end="")
+        print (random.choice(ranword))
+        print("Click Enter To Print Another Random Word")
+        input()
 def ranwordgen ():
     # using choice() to generate a random word from a
     # given list.
-    ranword = random.choice(['trash bin', 'hamer', 'window', 'mouse', 'brick','bin','happy','boy','super','ugly','man','woman','five','four','three','two'])
-    ranwordtext = Label(root, text=ranword,padx=50,pady=50,)
+    
+    ranwordtext = Label(root, text=random.choice(ranword),padx=50,pady=50,)
+    startbutton = Button(root, text="Genarate Random Word", height = 10, width =30,command=ranwordgen)
+    startbutton.grid(row=0, column=0)
     ranwordtext.grid(row=1, column=0)
+   
 
-#defines button
-startbutton = Button(root, text="Genarate Random Word", height = 10, width =30,command=ranwordgen)
-startbutton.grid(row=0, column=0)
-a = 1
-while a == 1:
-    print("A random word is : ", end="")
-    print(random.choice(ranword))
-    print("Click Enter To Print Another Random Word")
-    input()
+ranword = ['trash bin', 'hamer', 'window', 'mouse', 'brick','bin','happy','boy','super','ugly','man','woman','five','four','three','two']
+print("Type Console to use the consolo interface and type gui to use the gui interface")
+b = input()
+if b == "Console":
+    
+    consoleran()
+if b == "gui":
+    print("launching gui now")
+    ranwordgen()
+
+    
+
+
+
+
+
+
 
