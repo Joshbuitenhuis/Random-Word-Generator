a972333a5d2435e23bc5c61ee969a9f4139c093a
Thatoddone <66497499+Thatoddone1@users.noreply.github.com>
2020-09-17 07:24:38 -0400

    made some minner tweeks

--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 import random
 from tkinter import *
 root = Tk()
-root.title("Random Genarater")
+root.title("Random Word Generator")
 root.geometry("200x500")
 
 
@@ -18,13 +18,13 @@ def ranwordgen ():
     # given list.
     
     ranwordtext = Label(root, text=random.choice(ranword),padx=50,pady=50,)
-    startbutton = Button(root, text="Genarate Random Word", height = 10, width =30,command=ranwordgen)
+    startbutton = Button(root, text="Generate Random Word", height = 10, width =30,command=ranwordgen)
     startbutton.grid(row=0, column=0)
     ranwordtext.grid(row=1, column=0)
    
 
 ranword = ['trash bin', 'hamer', 'window', 'mouse', 'brick','bin','happy','boy','super','ugly','man','woman','five','four','three','two','one','six','why','how','boo','goo','fun','glue','pen','key','beg','I','mabey','so','no','yes','woo','yay','ok','go','nevermind','well','that','odd']
-print("Type Console to use the consolo interface and type gui to use the gui interface")
+print("Type Console to use the console interface and type gui to use the gui interface")
 b = input()
 if b == "Console":
     
